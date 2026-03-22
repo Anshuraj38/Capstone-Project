6ea211818d98a646d4df1a51c2e09063a110ff9a
DeepakRaj-02 <deepakraj200567@gmail.com>
2026-03-22 15:31:04 +0530

    changes in app.py and signup.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +8,12 @@ def index():
 @app.route('/register')
 def register():
     return render_template('signup.html')
+
+@app.route('/perform_registration' , methods=['POST'])
+def perform_registration():
+    name=request.form.get('username')
+    name=request.form.get('email')
+    name=request.form.get('password')
+    name=request.form.get('confirm_password')
+    return "registration successful"
 app.run(debug=True)
