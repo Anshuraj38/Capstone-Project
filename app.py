64b489d2a267dcd50b844a6458ddd4284fcc9bc0
Anshu Raj <rajanshu4444@gmail.com>
2026-04-11 15:19:53 +0530

    changes in app.py
    Added login perform and registration perform

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask , render_template , request
+from flask import Flask, render_template, request, session, redirect, url_for
 from db import Database
 app = Flask(__name__)
+
+app.secret_key = 'some_random_secret_key'
 
 dbo = Database()
 
@@ -12,16 +14,37 @@ def index():
 def register():
     return render_template('signup.html')
 
+@app.route('/dashboard')
+def dashboard():
+    name = session.get('name', 'User')
+    return render_template('dashboard.html', user_name=name)
+
 @app.route('/perform_registration' , methods=['POST'])
 def perform_registration():
     name = request.form.get('username')
     email = request.form.get('user_ka_email')
     password = request.form.get('user_ka_password')
     
-    response = dbo.insert(name,email,password)
+    response = dbo.insert(name, email, password)
 
     if response:
-        return "Registration Successful"
+        session['name'] = name
+        session['email'] = email
+        return redirect(url_for('dashboard'))
     else:
         return "Email already existed"
+
+@app.route('/perform_login', methods=['POST'])
+def perform_login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user_name = dbo.validate(email, password)
+    if user_name:
+        session['name'] = user_name
+        session['email'] = email
+        return redirect(url_for('dashboard'))
+    else:
+        return "Invalid email or password"
+
 app.run(debug=True)
--- db.py
+++ db.py
@@ -1,16 +1,25 @@
 import json
 
 class Database:
-    def insert(self,name,email,password):
+    def insert(self, name, email, password):
         with open('user.json', 'r') as rf:
             user = json.load(rf)
 
             if email in user:
                 return 0
             else:
-                user[email] = [name,password]
+                user[email] = [name, password]
 
-        with open('user.json','w') as wf:
-            json.dump(user,wf,indent=4)
+        with open('user.json', 'w') as wf:
+            json.dump(user, wf, indent=4)
             return 1
+
+    def validate(self, email, password):
+        with open('user.json', 'r') as rf:
+            user = json.load(rf)
+
+            if email in user and user[email][1] == password:
+                return user[email][0]
+
+        return None
 
